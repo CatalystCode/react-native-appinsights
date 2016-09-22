eecd54110460b3cf718fb923738a73f981b92bb1
Eric Rozell <erozell@outlook.com>
2016-09-22 15:58:39 -0400

    Adds initial implementation for AppInsights module for Windows

---
+++ windows/ReactWindowsApplicationInsights/ReactNativeApplicationInsightsPackage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace ReactWindowsApplicationInsights
+{
+    public class ReactNativeApplicationInsightsPackage : IReactPackage
+    {
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
+        {
+            return new List<Type>(0);
+        }
+
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>
+            {
+                new ReactNativeApplicationInsightsModule(),
+            };
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers(ReactContext reactContext)
+        {
+            return new List<IViewManager>(0);
+        }
+    }
+}
